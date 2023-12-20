d7decb7c1a5b3cac05f7ec03296eeb3836ecd054
Sami <saminachwati94@gmail.com>
2023-12-19 20:56:15 -0500

    Reading from file

--- imageSettings.php
+++ imageSettings.php
@@ -1,5 +1,19 @@
 <?php
 
-echo "Hi there";
+$imageFile = "imageFile.txt";
+
+$fh = fopen($imageFile, "r");
+$images = [];
+while(!feof($fh)){
+    $image = trim(fgets($fh));
+    array_push($image, $images);    
+}
+
+for($i=0; $i<count($images); $i++){
+
+    echo "<img src='" . $images[$i] . "'>"; 
+}
+
+
 
 ?>
